de8895b916bab067f3f7d0decf8dc41dcbc1c007
Gayaz Ahmed <110382271+fivetran-gayazahmed@users.noreply.github.com>
2024-01-11 20:40:17 +0530

    Update mongo_connector_sample.tf

--- sample database connectors/mongo_connector_sample.tf
+++ sample database connectors/mongo_connector_sample.tf
@@ -1,23 +1,25 @@
 # Mongo connector via Terraform
 
 resource "fivetran_connector" "mongo_via_terraform" {
-
-  group_id           = fivetran_destination.dest.group_id
+  group_id           = "your_group_id"
   service            = "mongo"
-  sync_frequency     = 5
-  paused             = false
-  pause_after_trial  = false
   run_setup_tests    = true
   trust_certificates = true
 
   destination_schema {
     prefix = "test_mongo_tf"
   }
-
   config {
-    hosts    = ["mongodb+srv://mongocluster01.1ipmzlz.mongodb.net/?retryWrites=true&w=majority"]
-    user     = "fivetran"
-    password = var.mongo_pass
+    hosts        = ["mongodb+srv://mongocluster01.1ipmzlz.mongodb.net/?retryWrites=true&w=majority"]
+    user         = "fivetran"
+    password     = var.mongo_password
+    packing_mode = "UsePackedModeOnly"
   }
+}
 
+resource "fivetran_connector_schedule" "mongo_connector_schedule" {
+  connector_id      = fivetran_connector.mongo_via_terraform.id
+  sync_frequency    = 60
+  paused            = false
+  pause_after_trial = false
 }
